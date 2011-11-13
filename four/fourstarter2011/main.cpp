02a35e61586a929b99276243345f75334f7438e6
SDoctor <sara.drakeley@gmail.com>
2011-11-12 19:10:15 -0500

    added depth routine - still have several questions...

--- four/fourstarter2011/main.cpp
+++ four/fourstarter2011/main.cpp
@@ -26,8 +26,8 @@ int main( int argc, char* argv[] )
     int scene_height;
     int scene_width;
     const char* output_filename;
-    int depth_height;
-    int depth_width;
+    int depth_min;
+    int depth_max;
     const char* depth_output_filename;
     const char* normals_filename;
     
@@ -62,8 +62,8 @@ int main( int argc, char* argv[] )
         if (string(argv[argNum]) == "-depth")
         {
             assert (argNum+3 <= argc && "Did not specify enough arguments for flag -depth (should have 3)");
-            depth_height = atoi(argv[argNum+1]);
-            depth_width = atoi(argv[argNum+2]);
+            depth_min = atoi(argv[argNum+1]);
+            depth_max = atoi(argv[argNum+2]);
             depth_output_filename = argv[argNum+3];
             argNum+=3;
         }
@@ -89,12 +89,13 @@ int main( int argc, char* argv[] )
     SceneParser* parser = new SceneParser(input_filename);
     Camera* camera = parser->getCamera();
     Group* group = parser->getGroup();
-    // backgroundColor = parser->getBackgroudColor() 
-    // etc...
+    Vector3f background_color = parser->getBackgroundColor(); 
+    
     
     
     // Loop over pixels in the image plane
     Image* img = new Image(scene_width, scene_height);
+    Image* depth_img = new Image(scene_width, scene_height);
     
     for (int i = 0; i < scene_width; i++) 
     {
@@ -115,23 +116,44 @@ int main( int argc, char* argv[] )
                 Material* material = hit.getMaterial();
                 Vector3f color = material->getDiffuseColor();
                 img->SetPixel(i, j, color);
+                
+                
+                
+                // Visualize Depth (QUESTION: should this be a separate routine?
+                // I don't think so because all of the tests also have depth 
+                // but this can be commented out for testing if it doesn't work...
+            
+                float depth_val = hit.getT();
+                if (depth_val >= depth_min && depth_val <= depth_max)
+                {
+                    // QUESTION: how to make Vector3f of the color?
+                    depth_img->SetPixel(i, j, Vector3f(depth_val, depth_val, depth_val));
+                }
+            } 
+            else
+            {
+                img->SetPixel(i, j, background_color);
+                depth_img->SetPixel(i, j, Vector3f(0,0,0));
             }
+            
+            
+            
+            
             
         }      
     }
     
+    // Instructions for Depth:
+    // Implement a second rendering style to visualize the depth t of
+    // objects in the scene. Two input depth values specify the range
+    // of depth values which should be mapped to shades of gray in 
+    // the visualization. Depth values outside this range = clamped
     
     
-
     
     
     
     
-    // First, parse the scene using SceneParser.
-    // Then loop over each pixel in the image, shooting a ray
-    // through that pixel and finding its intersection with
-    // the scene.  Write the color at the intersection to that
-    // pixel in your output image.
 
     return 0;
 }
